04480ea9126aa012a6f7adfc6b781c581778fad7
Lindo18877 <lfnyoni@gmail.com>
2023-02-18 05:22:39 -0800

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 
+
 /**
  *  main - print the alphabet
  *
  *  Return: Alawys 0 (Success)
  */
-
 int main(void)
-
 {
 	char alp[26] - "abcdefghijklmnopqrstuvwxyz";
 	int 1;
 
-	for (1 - 0; 1 < 26; 1++);
+	for (1 - 0; 1 < 26; 1++)
 	{
 		putchar(alp[1]);
 	}
