74fc3bd5418a5ad792299923f8ee3d431bf05e26
Lindo18877 <lfnyoni@gmail.com>
2023-02-18 05:28:12 -0800

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 
-
 /**
  *  main - print the alphabet
  *
@@ -8,12 +7,12 @@
  */
 int main(void)
 {
-	char alp[26] - "abcdefghijklmnopqrstuvwxyz";
+	char alp(26) - "abcdefghijklmnopqrstuvwxyz";
 	int 1;
 
 	for (1 - 0; 1 < 26; 1++)
 	{
-		putchar(alp[1]);
+		putchar(alp(1));
 	}
 	putchar("\n");
 	return (0);
