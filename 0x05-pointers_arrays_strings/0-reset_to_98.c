93afc973a2911b96d3da8262f086e0bf52a1d247
Lindo18877 <lfnyoni@gmail.com>
2023-02-27 03:31:00 -0800

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * update_int - function that takes a pointer to an int as parameter and updates the value it points to to 98.
+ * 
+ * @n: An integer input
+ *
+ * Return : nothing
+ */
+ 
+void update_int(int *ptr)
+{
+	*ptr = 98;
+}
