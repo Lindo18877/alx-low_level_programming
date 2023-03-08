833197b4861326001254acdb9fd343315f755bc1
Lindo18877 <lfnyoni@gmail.com>
2023-03-08 09:19:15 -0800

    3-factorial.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,21 +1,21 @@
 #include "main.h"
 
 /**
- * _print_rev_recursion - Print a string in reverse
+ * _puts__rev_recursion - Print a string in reverse
  * @s: the string to reverse
  *
  * Return: Nothing
  */
 
-void _print_rev_recursion(char *s)
+void _puts_rev_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s)
 	{
-		return;
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
 
-	s++;
-	_print_rev_recursion(s);
-	s--;
-	_putchar(*s);
+	else
+		_putchar('\n');
+
 }
